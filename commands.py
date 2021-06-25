d0d22aca090c3dff84145d14f10e8bbe9705cf34
swill <swilltec@gmail.com>
2021-06-25 01:51:56 +0000

    feat: add creatbookmarks table command

---
+++ commands.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+from database import DatabaseManager
+
+
+db = DatabaseManager('bookmarks.db')
+
+class CreateBookMarksTableCommand:
+    def execute(self):
+        db.create_table('bookmarks', {
+            'id': 'integer primary key autoincrement',
+            'title': 'text not null',
+            'url': 'text not null',
+            'notes': 'text',
+            'date_added': 'text not null'
+        })
+
+
